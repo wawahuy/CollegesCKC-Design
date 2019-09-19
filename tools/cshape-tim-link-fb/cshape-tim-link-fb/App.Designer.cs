9188841a79f9863c45e793eada5f749fdbc32995
wawahuy <kakahuy99@gmail.com>
2019-09-20 00:20:12 +0700

    .

--- tools/cshape-tim-link-fb/cshape-tim-link-fb/App.Designer.cs
+++ tools/cshape-tim-link-fb/cshape-tim-link-fb/App.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(584, 383);
             this.Name = "App";
             this.Text = "Tìm link fb";
             this.ResumeLayout(false);
--- web/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-      ///header("Status: 301 Moved Permanently");
-      ///header("Location: http://localhost:8000/fb/login/callback?". $_SERVER['QUERY_STRING']);
-      exit;
-?>
